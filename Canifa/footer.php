52b3bdd1ef5e28ec7fab77ab71cd24a8b144b491
quangtom9121 <tranquangntch@gmail.com>
2023-06-19 01:15:50 +0700

    commit

---
+++ Canifa/footer.php
@@ -0,0 +1,39 @@
+    <!-- -------------------------Footer -->
+    <section class="footer">
+        <div class="footer-container">
+            <p>Tải ứng dụng CANIFA</p>
+            <div class="app-google">
+                <a href=""><img src="image/appstore.jpg" alt=""></a>
+                <a href=""> <img src="image/googleplay.jpg" alt=""></a>
+            </div>
+            <p>Nhận bản tin CANIFA</p>
+            <div class="input-email">
+                <input type="text" placeholder="Nhập email của bạn">
+                <i class="fas fa-arrow-left"></i>
+            </div>
+            <div class="footer-items">
+                <li><a href=""><img src="image/dathongbao.png" alt=""></a></li>
+                <li><a href="">Liên hệ</a></li>
+                <li><a href="">Tuyển dụng</a></li>
+                <li><a href="">Giới thiệu</a></li>
+                <li><a href=""><i class="fab fa-facebook-f"></i></a><a href=""><i class="fab fa-youtube"></i></a></li>
+            </div>
+            <div class="footer-text">
+            Công ty Cổ phần CANIFA với số đăng ký kinh doanh: 0107574310 <br>
+Trụ sở chính: Số 688, Đường Quang Trung, Phường La Khê, Quận Hà Đông, Hà Nội, Việt Nam <br>
+Địa chỉ liên hệ: Phòng 301 Tòa nhà GP Invest, 170 La Thành, Phường Ô Chợ Dừa, Quận Đống Đa, Thành phố Hà Nội <br>
+Số điện thoại: +8424 - 7303.0222
+            </div>
+            <div class="footer-bottom"> 
+            © 2022 CANIFA
+            </div>
+        </div>
+    </section>
+
+
+<script src="js/script.js"></script>
+<script src="js/slider.js"></script>
+
+
+</body>
+</html>
